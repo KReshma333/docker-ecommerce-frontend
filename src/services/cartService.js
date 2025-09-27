41055151996a3ab5bf9f276a1dac01738cc35f33
Reshma Chowdary Kommi <reshmakommi18@gmail.com>
2025-09-27 09:50:55 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://3.17.73.247:8081/api/cart";
+const API_URL = "http://3.139.61.101:8081/api/cart";
 
-const API_BASE = "http://3.17.73.247:8081/api/cart";
+const API_BASE = "http://3.139.61.101:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
